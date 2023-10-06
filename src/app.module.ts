a7c4848b0052dc701c96b9acd82f32d35c2b80f7
Jean Castro <61846534+Jeanlucascastro@users.noreply.github.com>
2023-10-05 22:47:15 -0300

    arquivo .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,15 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CourseModule } from './course/course.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { TypeormService } from './config/typeorm/typeorm.service';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [CourseModule,
-    TypeOrmModule.forRootAsync({
-      useClass: TypeormService,
-    }),
+    TypeOrmModule.forRoot({
+      type: process.env.TYPEORM_CONNECTION,
+      host: process.env.TYPEORM_HOST,
+      port: process.env.TYPEORM_PORT,
+      username: process.env.TYPEORM_USERNAME,
+      password: process.env.TYPEORM_PASSWORD,
+      database: process.env.TYPEORM_DATABASE,
+      entities: [__dirname + '/**/*.entity{.js,.ts}'],
+      synchronize: true,
+    } as TypeOrmModuleOptions),
     ConfigModule.forRoot(),
     CourseModule,],
   controllers: [AppController],
--- src/config/typeorm/typeorm.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { TypeormService } from './typeorm.service';
-
-@Module({
-  imports: [ConfigModule.forRoot()],
-  providers: [TypeormService],
-})
-export class TypeormModule {}
